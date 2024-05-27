fc2c663a5ed1933c369c5f5fac77565e663197c9
anjaemun <ajm0111@naver.com>
2024-05-27 22:20:18 +0900

    second,third

--- python_1.py
+++ python_1.py
@@ -1,6 +1,6 @@
 Python 3.12.3 (v3.12.3:f6650f9ad7, Apr  9 2024, 08:18:47) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
 Type "help", "copyright", "credits" or "license()" for more information.
->>> n = 10
+n = 10
 >>> if n%2 == 0
 SyntaxError: expected ':'
 >>> if n%2 == 0;
@@ -28,3 +28,15 @@ NameError: name 'b' is not defined
 >>> a = 2
 >>> print(n+a)
 12
+>>> #함수 이름(인자, 인자, 인자) => 리턴
+>>> print('hi','hello')
+hi hello
+>>> input('이름을 입력하세요')
+이름을 입력하세요안재문
+'안재문'
+>>> name = input('이름을 입력하세요 : ')
+이름을 입력하세요 : 재문입니다
+>>> print(name)
+재문입니다
+>>> print('안녕하세요',name)
+안녕하세요 재문입니다
